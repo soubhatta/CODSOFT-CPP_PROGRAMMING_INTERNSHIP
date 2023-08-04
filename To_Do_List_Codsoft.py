937d3860cd5a7d71f644e74f64f16fb49735f0e3
Soumalya Bhattacharyya <122465630+soubhatta@users.noreply.github.com>
2023-08-04 15:40:51 +0530

    Update To_Do_List_Codsoft.py

--- To_Do_List_Codsoft.py
+++ To_Do_List_Codsoft.py
@@ -1,8 +1,7 @@
 # CodSoft Internship
-# Domain Name - Python 
-# A To-Do List application is a useful project that helps users manage and organize their tasks efficiently. This project aims to create a command-line or GUI-based application using Python, allowing users to create, update, and track their to-do lists
-# TASK - 2(TO-DO-LIST)
-# INTERN NAME - SOUMALYA BHATTACHARYA
+# Domain Name - Python Programming
+# Task - 2(To Do List)
+# Intern Name - Soumalya Bhattacharyya
 
 import tkinter as tk
 from tkinter import messagebox
